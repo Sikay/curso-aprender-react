9dacd74c80c259d3c36f586d19d15ac6aba3db73
Sikay <pedrohdezmora@gmail.com>
2020-03-13 20:00:40 +0100

    State I Gestion del estado en react

--- src/State.js
+++ src/State.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 
+class Contador extends Component {
+  constructor() {
+    super();
+    this.state = { contador: 1 };
+  }
+  render() {
+    return <span>{this.state.contador}</span>;
+  }
+}
+
 class State extends Component {
   render() {
     return (
       <div>
         <p>Primer componente con state</p>
+        <Contador />
       </div>
     );
   }
