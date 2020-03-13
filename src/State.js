5667e600475da34562a3f89c802ae18628d6cdad
Sikay <pedrohdezmora@gmail.com>
2020-03-13 19:57:10 +0100

    State I Archivo inicial + render

---
+++ src/State.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class State extends Component {
+  render() {
+    return (
+      <div>
+        <p>Primer componente con state</p>
+      </div>
+    );
+  }
+}
+
+export default State;
