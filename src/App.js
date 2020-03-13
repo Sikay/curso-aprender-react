d9988137be9b2b68a9cdd8eff90aa681a489c166
Sikay <pedrohdezmora@gmail.com>
2020-03-13 19:23:13 +0100

    Props I

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class Text extends Component {
   render() {
     // Podemos renderizar variables
     const textoSegunBool = this.props.boolean ? "Verdadero" : "Falso";
+    const mappedNumbers = this.props.arrayOfNumbers.map(n => n * 2);
     return (
       <div>
         <p>{this.props.text}</p>
@@ -17,6 +18,8 @@ class Text extends Component {
         <p>{this.props.boolean}</p>
         <p>{JSON.stringify(this.props.boolean)}</p>
         <p>{textoSegunBool}</p>
+        <p>{mappedNumbers.join(", ")}</p>
+        <p>{this.props.objectWithInfo.key}</p>
       </div>
     );
   }
@@ -29,7 +32,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Hello title="Welcome to React" />
-          <Text text="Esto es un texto" number={2} boolean={true} />
+          <Text
+            arrayOfNumbers={[2, 3, 10]}
+            boolean={true}
+            number={2}
+            objectWithInfo={{ key: "value", key2: "otherValue" }}
+            text="Esto es un texto"
+          />
           <a
             className="App-link"
             href="https://reactjs.org"
