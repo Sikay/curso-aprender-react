17c415d83e9fdd19d705524b8a4d1eaad793b594
Sikay <pedrohdezmora@gmail.com>
2020-03-15 13:05:40 +0100

    Eventos sinteticos

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 
 class Formulario extends Component {
+  handleClick(e) {
+    // React envuelve los eventos para que sean complatible con los navegadores
+    console.log(e);
+    console.log(e.nativeEvent);
+    alert("hi");
+  }
   render() {
     return (
       <div>
         <h4>Eventos</h4>
-        <button onClick={() => alert("hi")}>Hi there!</button>
+        <button onClick={this.handleClick}>Hi there!</button>
       </div>
     );
   }
