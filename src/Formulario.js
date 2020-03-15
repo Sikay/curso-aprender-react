173f7ebcf14bd2cc1848a3e9b200a0355f1678ea
Sikay <pedrohdezmora@gmail.com>
2020-03-15 13:20:21 +0100

    Eventos soportados y bind

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
+import Forms from "./sections/Forms";
 
 class Formulario extends Component {
+  constructor() {
+    super();
+    this.state = { mouseX: 0, mouseY: 0 };
+    // this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+  // Las arrow function enlazan simpre el contexto desde donde se las llama
+  handleMouseMove = e => {
+    const { clientX, clientY } = e;
+    this.setState({
+      mouseX: clientX,
+      mouseY: clientY
+    });
+  };
   handleClick(e) {
     // React envuelve los eventos para que sean complatible con los navegadores
     console.log(e);
@@ -12,6 +26,19 @@ class Formulario extends Component {
       <div>
         <h4>Eventos</h4>
         <button onClick={this.handleClick}>Hi there!</button>
+        <div
+          onMouseMove={this.handleMouseMove}
+          style={{
+            border: "1px solid black",
+            margin: "10px 0 0 0",
+            padding: "10px"
+          }}
+        >
+          <p>
+            {this.state.mouseX}, {this.state.mouseY}
+          </p>
+        </div>
+        <Forms />
       </div>
     );
   }
---
+++ src/sections/Forms.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Forms extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Formulario</h4>
+      </div>
+    );
+  }
+}
+
+export default Forms;
