d297175a277c472d5e46e1c1a0d1f4e78c52c968
Sikay <pedrohdezmora@gmail.com>
2020-03-15 14:39:16 +0100

    Refactorizacion

--- src/App.js
+++ src/App.js
@@ -1,73 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
-const Hello = props => <h2>{props.title}</h2>;
-
-class Text extends Component {
-  // render solo puede devolver un elemento por lo que todos los elementos
-  // que utilicemos tendran que estar envueltos
-  render() {
-    // Las props son inmutables
-    // Destructuracion
-    const {
-      arrayOfNumbers,
-      boolean,
-      number,
-      multiply,
-      objectWithInfo,
-      text,
-      title,
-      propPorDefecto
-    } = this.props;
-    // Podemos renderizar variables
-    const textoSegunBool = boolean ? "Verdadero" : "Falso";
-    const mappedNumbers = arrayOfNumbers.map(multiply);
-    return (
-      <div>
-        {title}
-        <p>{propPorDefecto}</p>
-        <p>{text}</p>
-        <p>{number}</p>
-        <p>{boolean}</p>
-        <p>{JSON.stringify(boolean)}</p>
-        <p>{textoSegunBool}</p>
-        <p>{mappedNumbers.join(", ")}</p>
-        <p>{objectWithInfo.key}</p>
-      </div>
-    );
-  }
-}
-
-Text.defaultProps = {
-  propPorDefecto: "Texto por defecto"
-};
+import HelloWorld from "./HelloWorld";
+import State from "./State";
+import Condicional from "./Condicional";
+import Lista from "./Lista";
+import Formulario from "./Formulario";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Hello title="Welcome to React" />
-          <Text
-            arrayOfNumbers={[2, 3, 10]}
-            boolean={true}
-            number={2}
-            multiply={number => number * 4}
-            objectWithInfo={{ key: "value", key2: "otherValue" }}
-            text="Esto es un texto"
-            title={<h1>Este es el titulo</h1>}
-          />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <HelloWorld />
+        <State />
+        <Condicional />
+        <Lista />
+        <Formulario />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import State from "./State";
-import Condicional from "./Condicional";
-import Lista from "./Lista";
-import Formulario from "./Formulario";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(<State />, document.getElementById("root"));
-ReactDOM.render(<Condicional />, document.getElementById("root"));
-ReactDOM.render(<Lista />, document.getElementById("root"));
-ReactDOM.render(<Formulario />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
